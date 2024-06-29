90aa313a891e73bbc71ffb205bde89d5e5a416cf
Ankush073 <ankush.ankush2414@gmail.com>
2024-06-29 18:02:22 +0530

    commit all changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response) => {
+app.use("/", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
 
